f87305facf3713ac52c3d5c2ec6392276afeaf7d
Ayush-neu23 <144970097+Ayush-neu23@users.noreply.github.com>
2024-02-21 20:58:59 -0500

    commit (#6)

--- main.tf
+++ main.tf
@@ -8,24 +8,24 @@ resource "google_compute_network" "vpc_1" {
   name                    = var.vpc_name
   auto_create_subnetworks = false
   delete_default_routes_on_create = true
-  routing_mode            = "REGIONAL"
+  routing_mode       = var.routing_mode 
 }
 
 resource "google_compute_subnetwork" "webapp" {
   name          = var.subnet1
-  ip_cidr_range = "10.0.1.0/24"
+  ip_cidr_range = var.cidr1
   network       = google_compute_network.vpc_1.self_link
 }
 
 resource "google_compute_subnetwork" "db" {
   name          = var.subnet2
-  ip_cidr_range = "10.0.2.0/24"
+  ip_cidr_range = var.cidr2
   network       = google_compute_network.vpc_1.self_link
 }
 
 resource "google_compute_route" "route_1" {
   name                   = var.route_name
   network                = google_compute_network.vpc_1.self_link
-  dest_range             = "0.0.0.0/0"
-  next_hop_gateway  = "default-internet-gateway"
+  dest_range             = var.dest_range
+  next_hop_gateway  = var.next_hop_gateway
 }
