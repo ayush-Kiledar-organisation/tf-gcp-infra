c728a43b8316419948664660844b4e124cb35d9a
Ayush Kiledar <kiledar.ay@northeastern.edu>
2024-02-21 20:45:16 -0500

    commit

--- variables.tf
+++ variables.tf
@@ -41,3 +41,35 @@ variable "subnet2" {
   description = "Subnet 2"
   default = "db"
 }
+
+variable "cidr1" {
+  type = string
+  description = "range 1"
+  default = "10.0.1.0/24"
+}
+
+variable "cidr2" {
+  type = string
+  description = "range 2"
+  default = "10.0.2.0/24" 
+}
+
+variable routing_mode {
+  type = string
+  description = "routing mode of VPC"
+  default = "REGIONAL"
+}
+
+variable "dest_range" {
+  type = string
+  description = "destination range"
+  default = "0.0.0.0/0"
+  
+}
+
+variable "next_hop_gateway" {
+  type = string
+  description = "next hop gateway"
+  default = "default-internet-gateway"
+  
+}
