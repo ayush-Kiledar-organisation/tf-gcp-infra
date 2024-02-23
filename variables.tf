afdaf74f975a203648a4064594f22b9dea9c094f
Ayush-neu23 <144970097+Ayush-neu23@users.noreply.github.com>
2024-02-22 21:58:31 -0500

    Tf21 (#7)
    * commit
    
    * Update main.tf
    
    * Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,45 +1,33 @@
-variable "projecy_id" {
-    type = string
-  description = "The ID of current project"
-  default = "terraform-assignment-414415"
-  
+#variables.tf
+
+variable "project_id" {
+  type = string
+  default = "dev-assignment4"
+  description = "Project ID"
 }
 
 variable "region" {
   type = string
-  description = "The region of the resources"
-  default = "us-central1" 
+  default = "us-central1"
+  description = "The region"
+}
+
+variable "auto_create_subnetworks" {
+  type = bool
+  default = false
+  description = "Auto Create subnetworks"
+}
+
+variable "routing_mode" {
+  type = string
+  description = "routing mode of VPC"
+  default = "REGIONAL"
 }
 
 variable "zone" {
   type = string
   description = "The zone of the resources"
   default = "us-central1-c"
-  
-}
-
-variable "vpc_name" {
-  type = string
-  description = "The name of the VPC"
-  default = "vpc-1" 
-}
-
-variable "route_name" {
-  type = string
-  description = "The name of the route"
-  default = "route-1"  
-}
-
-variable "subnet1" {
-  type = string
-  description = "Subnet 1"
-  default = "webapp"  
-}
-
-variable "subnet2" {
-  type = string
-  description = "Subnet 2"
-  default = "db"
 }
 
 variable "cidr1" {
@@ -47,29 +35,107 @@ variable "cidr1" {
   description = "range 1"
   default = "10.0.1.0/24"
 }
-
 variable "cidr2" {
   type = string
   description = "range 2"
   default = "10.0.2.0/24" 
 }
 
-variable routing_mode {
+variable "route_name" {
   type = string
-  description = "routing mode of VPC"
-  default = "REGIONAL"
+  description = "The name of the route"
+  default = "route-1" 
 }
 
 variable "dest_range" {
   type = string
   description = "destination range"
   default = "0.0.0.0/0"
+}
+
+variable "vm_instance_name" {
+  type = string
+  description = "The name of the VM instance"
+  default = "webapp-instance"
+}
+
+variable "machine_type" {
+  type = string
+  description = "The machine type of the VM instance"
+  default = "n1-standard-1"
+}
+
+variable "image_name" {
+  type = string
+  description = "The name of the custom image"
+  default = "myimage2"
+}
+
+variable "image_size" {
+  default = "100"
+}
+
+variable "image_type" {
+  default = "pd-standard"
+}
+
+variable "firewall_deny" {
+  default = ["22"]
+}
+
+variable "firewall_allow" {
+  default = ["3000", "8080"]
+}
+
+variable "tcp" {
+  type = string
+  default = "tcp"
+  description = "name of tcp"
+}
+
+variable "udp" {
+  type = string
+  default = "udp"
+  description = "name of udp"
   
 }
 
-variable "next_hop_gateway" {
+variable "app_name" {
   type = string
-  description = "next hop gateway"
-  default = "default-internet-gateway"
+  default = "webapp"
+  description = "The name of the app"
   
+}
+
+variable "vpcs" {
+  description = "list of all vpc"
+  type        = list(string)
+  default     = ["new-vpc"]
+}
+
+variable "db_name" {
+  type = string
+  default = "db"
+  description = "name of db"
+  
+}
+
+variable "gateway" {
+  type = string
+  default = "default-internet-gateway"
+  description = "default-internet-gateway"
+  
+}
+
+variable "firname" {
+  type = string
+  default = "allow-request"
+  description = "name of firewall"
+  
+}
+
+variable "deny_tcp" {
+  type = string
+  default = "deny-all"
+  description = "name of deny firewall"
 }
