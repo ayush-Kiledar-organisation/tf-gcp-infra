33c2e613b22b800840e6f8c2f5ddd5ba9073fda5
Ayush-neu23 <144970097+Ayush-neu23@users.noreply.github.com>
2024-02-22 21:55:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,99 @@
 provider "google" {
-  project = var.projecy_id
-  region = var.region
-  zone = var.zone
+  project     = var.project_id
+  region      = var.region
 }
 
-resource "google_compute_network" "vpc_1" {
-  name                    = var.vpc_name
-  auto_create_subnetworks = false
+
+resource "google_compute_firewall" "allow_request" {
+
+  for_each = google_compute_network.vpc
+  name     = var.firname
+  network  = each.value.self_link
+
+  allow {
+    protocol = var.tcp
+    ports    = var.firewall_allow
+  }
+
+  source_ranges = [var.dest_range]
+
+  target_tags = ["${each.key}-${var.app_name}", "http-server"]
+}
+
+resource "google_compute_firewall" "deny_tcp" {
+  for_each = google_compute_network.vpc
+  name     = var.deny_tcp
+  network  = each.value.self_link
+
+  deny {
+    protocol = var.tcp
+    ports    = var.firewall_deny
+  }
+
+  deny {
+    protocol = var.udp
+    ports    = var.firewall_deny
+  }
+
+  source_ranges = [var.dest_range]
+
+  target_tags = ["${each.key}-${var.app_name}"]
+
+}
+
+resource "google_compute_network" "vpc" {
+  for_each                        = toset(var.vpcs)
+  name                            = each.key
+  auto_create_subnetworks         = var.auto_create_subnetworks
+  routing_mode                    = var.routing_mode
   delete_default_routes_on_create = true
-  routing_mode       = var.routing_mode 
 }
 
 resource "google_compute_subnetwork" "webapp" {
-  name          = var.subnet1
+  for_each      = google_compute_network.vpc
+  name          = "${each.key}-${var.app_name}"
   ip_cidr_range = var.cidr1
-  network       = google_compute_network.vpc_1.self_link
+  network       = each.value.self_link
+  region        = var.region
 }
 
 resource "google_compute_subnetwork" "db" {
-  name          = var.subnet2
+  for_each      = google_compute_network.vpc
+  name          = "${each.key}-${var.db_name}"
   ip_cidr_range = var.cidr2
-  network       = google_compute_network.vpc_1.self_link
+  network       = each.value.self_link
+  region        = var.region
 }
 
-resource "google_compute_route" "route_1" {
-  name                   = var.route_name
-  network                = google_compute_network.vpc_1.self_link
-  dest_range             = var.dest_range
-  next_hop_gateway  = var.next_hop_gateway
+resource "google_compute_route" "webapp" {
+  for_each         = google_compute_network.vpc
+  name             = "${each.key}-route"
+  dest_range       = var.dest_range
+  network          = each.value.name
+  next_hop_gateway = var.gateway
+  priority         = 1000
+  tags             = ["${each.key}-${var.app_name}"]
+}
+
+resource "google_compute_instance" "vm_instance_webapp" {
+  for_each     = google_compute_network.vpc
+  name         = var.vm_instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "projects/${var.project_id}/global/images/${var.image_name}"
+      size  = var.image_size
+      type  = var.image_type
+    }
+  }
+
+  network_interface {
+    network    = each.value.self_link
+    subnetwork = google_compute_subnetwork.webapp[each.key].self_link
+    access_config {
+    }
+  }
+  tags = ["${each.key}-${var.app_name}", "http-server"]
 }
