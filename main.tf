16de374d57b22a404a14409a9f2e4911b79cf246
Ayush Kiledar <kiledar.ay@northeastern.edu>
2024-02-26 01:21:56 -0500

    commit

--- main.tf
+++ main.tf
@@ -5,10 +5,8 @@ provider "google" {
 
 
 resource "google_compute_firewall" "allow_request" {
-
-  for_each = google_compute_network.vpc
   name     = var.firname
-  network  = each.value.self_link
+  network  = google_compute_network.vpc.self_link
 
   allow {
     protocol = var.tcp
@@ -17,13 +15,12 @@ resource "google_compute_firewall" "allow_request" {
 
   source_ranges = [var.dest_range]
 
-  target_tags = ["${each.key}-${var.app_name}", "http-server"]
+  target_tags = ["${var.vpc}-${var.app_name}", "http-server"]
 }
 
 resource "google_compute_firewall" "deny_tcp" {
-  for_each = google_compute_network.vpc
   name     = var.deny_tcp
-  network  = each.value.self_link
+  network  = google_compute_network.vpc.self_link
 
   deny {
     protocol = var.tcp
@@ -37,64 +34,124 @@ resource "google_compute_firewall" "deny_tcp" {
 
   source_ranges = [var.dest_range]
 
-  target_tags = ["${each.key}-${var.app_name}"]
+  target_tags = ["${var.vpc}-${var.app_name}"]
 
 }
 
 resource "google_compute_network" "vpc" {
-  for_each                        = toset(var.vpcs)
-  name                            = each.key
+  provider                =   google-beta
+  name                            = var.vpc
   auto_create_subnetworks         = var.auto_create_subnetworks
   routing_mode                    = var.routing_mode
   delete_default_routes_on_create = true
-
 }
 
 resource "google_compute_subnetwork" "webapp" {
-  for_each      = google_compute_network.vpc
-  name          = "${each.key}-${var.app_name}"
+  provider                = google-beta
+  project = google_compute_network.vpc.project
+  name          = "${var.vpc}-${var.app_name}"
   ip_cidr_range = var.cidr1
-  network       = each.value.self_link
+  network       = google_compute_network.vpc.self_link
   region        = var.region
+  private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "db" {
-  for_each      = google_compute_network.vpc
-  name          = "${each.key}-${var.db_name}"
+  provider                = google-beta
+  project = google_compute_network.vpc.project
+  name          = "${var.vpc}-${var.db_name}"
   ip_cidr_range = var.cidr2
-  network       = each.value.self_link
+  network       = google_compute_network.vpc.self_link
   region        = var.region
+  private_ip_google_access = true
 }
+# add-ons
+
+resource "google_compute_global_address" "private_ip_address" {
+  name         = "private-ip-address"
+  address_type = "INTERNAL"
+  purpose      = "VPC_PEERING"
+  network      = google_compute_network.vpc.id
+  prefix_length = 16
+}
+# resource "google_compute_global_forwarding_rule" "default" {
+#   provider              = google-beta
+#   project               = google_compute_network.vpc.project
+#   name                  = "globalrule"
+#   target                = "all-apis"
+#   network               = google_compute_network.vpc.id
+#   ip_address            = google_compute_global_address.default.id
+#   load_balancing_scheme = ""
+# }
 
 resource "google_compute_route" "webapp" {
-  for_each         = google_compute_network.vpc
-  name             = "${each.key}-route"
+  name             = "${var.vpc}-route"
   dest_range       = var.dest_range
-  network          = each.value.name
+  network          = google_compute_network.vpc.name
   next_hop_gateway = var.gateway
   priority         = 1000
-  tags             = ["${each.key}-${var.app_name}"]
+  tags             = ["${var.vpc}-${var.app_name}"]
 }
 
-resource "google_compute_instance" "vm_instance_webapp" {
-  for_each     = google_compute_network.vpc
-  name         = var.vm_instance_name
-  machine_type = var.machine_type
-  zone         = var.zone
+# resource "google_compute_instance" "vm_instance_webapp" {
+#   for_each     = google_compute_network.vpc
+#   name         = var.vm_instance_name
+#   machine_type = var.machine_type
+#   zone         = var.zone
 
-  boot_disk {
-    initialize_params {
-      image = "projects/${var.project_id}/global/images/${var.image_name}"
-      size  = var.image_size
-      type  = var.image_type
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "projects/${var.project_id}/global/images/${var.image_name}"
+#       size  = var.image_size
+#       type  = var.image_type
+#     }
+#   }
 
-  network_interface {
-    network    = each.value.self_link
-    subnetwork = google_compute_subnetwork.webapp[each.key].self_link
-    access_config {
+#   network_interface {
+#     network    = each.value.self_link
+#     subnetwork = google_compute_subnetwork.webapp[each.key].self_link
+#     access_config {
+#     }
+#   }
+#   tags = ["${each.key}-${var.app_name}", "http-server"]
+# }
+
+resource "google_sql_database_instance" "db_instance" {
+  name = "db-instance"
+  database_version = "MYSQL_8_0"
+  region             = var.region
+  deletion_protection = false
+  settings {
+    tier = "db-f1-micro"
+    disk_type = var.disk_type
+    disk_size = var.disk_size
+    ip_configuration {
+      ipv4_enabled = false
+      private_network = google_compute_network.vpc.self_link
     }
+    backup_configuration {
+      enabled = true
+      binary_log_enabled = true
+    }
+    availability_type = var.availability_type
+    
+  
   }
-  tags = ["${each.key}-${var.app_name}", "http-server"]
+}
+
+resource "google_sql_database" "database" {
+  name     = "webapp"
+  instance = google_sql_database_instance.db_instance.name
+}
+
+resource "google_sql_user" "db_user" {
+  name     = "webapp"
+  instance = google_sql_database_instance.db_instance.name
+  password = random_password.password.result
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
 }
